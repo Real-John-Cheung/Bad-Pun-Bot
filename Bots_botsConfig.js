cf60947c2c53773b1e8422a1f50ad589ce1c636b
張喻 <zhangyu@zhangyude-MacBook-Pro.local>
2020-04-11 15:42:57 +0800

    Changed route response

---
+++ Bots_botsConfig.js
@@ -0,0 +1,10 @@
+if (process.env.CONSUMER_KEY == undefined){
+  require('./env.js');
+}
+module.exports = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  verbose: true
+}
